02b1d10c906e5effb56c1146e3e70244be3f42dc
Punith1117 <punith11172006@gmail.com>
2025-03-04 16:26:35 +0530

    increase height of textarea by overriding default size

--- src/components/SummaryInput.jsx
+++ src/components/SummaryInput.jsx
@@ -2,7 +2,7 @@ export default function SummaryInput({summary, setSummary}) {
     return (
         <section className="input-summary input-section">
             <h1>Summary: </h1>
-            <textarea value={summary} onChange={(e) => setSummary(e.target.value)}></textarea>
+            <textarea value={summary} onChange={(e) => setSummary(e.target.value)} style={{height: '10rem'}}></textarea>
         </section>
     )
 }
